6dff65dc72228437824d181c183f22919de1d087
“eonxonee” <b2029amvveloso@pshs.edu.ph>
2025-09-06 21:47:16 +0800

    Add logic to evaluate and classify a number as "Weird" or "Not Weird"

--- public/Q15GPotassiumVeloso.js
+++ public/Q15GPotassiumVeloso.js
@@ -1,2 +1,18 @@
+process.stdin.on('data', input => {
 
+  const NUM = Number(input.toString().trim());
+  // use NUM variable in your code and start it on the next line
 
+  if (NUM % 2 === 1) {
+    console.log("Weird");
+  } else if (NUM >= 2 && NUM <= 5) {
+    console.log("Not Weird");
+  } else if (NUM >= 6 && NUM <= 20) {
+    console.log("Weird");
+  } else if (NUM > 20) {
+    console.log("Not Weird");
+  }
+
+  // end of your code
+
+  process.exit(); // ends the code
